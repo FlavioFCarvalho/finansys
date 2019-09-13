1433eb8e0f3c527e740d707213001115ed8e4e14
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-13 10:31:37 -0300

    Configurando os Gráficos dos Relatórios

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,15 +3,17 @@ import { Injectable, Injector } from '@angular/core';
 import { BaseResourceService } from 'src/app/shared/service/base-resource.service';
 import { Entry } from "./entry.model";
 import { CategoryService } from '../../categories/shared/category.service';
-import { flatMap, catchError } from 'rxjs/operators';
+import { flatMap, catchError, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { renderTemplate } from '@angular/core/src/render3/instructions';
+
+import * as moment from "moment";
 
 
 @Injectable({
     providedIn: 'root'
 })
 export class EntryService extends BaseResourceService<Entry>{
+
 
     constructor(protected injector: Injector,
         private categoryService: CategoryService) {
@@ -35,6 +37,22 @@ export class EntryService extends BaseResourceService<Entry>{
             }),
             catchError(this.handleError)
         )
+    }
+
+    getByMonthAndYear(month: number, year: number) {
+        return this.getAll().pipe(
+            map(entries => this.filterByMonthAndYear(entries, month, year))
+        )
+    }
+
+    private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+        return entries.filter(entry => {
+            const entryDate = moment(entry.date, "DD/MM/YYYY");
+            const monthMatches = entryDate.month() + 1 == month;
+            const yearMatches = entryDate.year() + 1 == year;
+
+            if (monthMatches && yearMatches) return entry;
+        })
     }
 
 }
