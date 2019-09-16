02747312d65afb27e5e3f246e7bd75c2649735a5
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-16 17:09:09 -0300

    Criação do compoment partner-list e inclusão BaseResourceListComponent

---
+++ src/app/pages/partners/partner-list/partner-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { BaseResourceListComponent } from '../../../shared/base-resource-list/base-resource-list.component';
+import { Partner } from '../shared/partner.model';
+import { PartnerService } from '../shared/partner.service';
+
+@Component({
+  selector: 'app-partner-list',
+  templateUrl: './partner-list.component.html',
+  styleUrls: ['./partner-list.component.css']
+})
+export class PartnerListComponent extends BaseResourceListComponent<Partner> {
+
+  get partners() {
+    return this.resources;
+  }
+
+
+  constructor(private partnerService: PartnerService) {
+    super(partnerService);
+  }
+
+
+}
