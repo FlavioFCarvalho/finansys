f041641babdccb58f5bf8c6693c8b54cf39b1e54
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-17 08:34:48 -0300

    Criação do Partner-form

--- src/app/pages/partners/partners-routing.module.ts
+++ src/app/pages/partners/partners-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PartnerListComponent } from './partner-list/partner-list.component';
+import { PartnerFormComponent } from './partner-form/partner-form.component';
 
 const routes: Routes = [
-  { path: '', component: PartnerListComponent }
+  { path: '', component: PartnerListComponent },
+  { path: 'new', component: PartnerFormComponent },
+  { path: ':id/edit', component: PartnerFormComponent }
 ];
 
 @NgModule({
--- src/app/pages/partners/partners.module.ts
+++ src/app/pages/partners/partners.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { PartnersRoutingModule } from './partners-routing.module';
 import { PartnerListComponent } from './partner-list/partner-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PartnerFormComponent } from './partner-form/partner-form.component';
 
 @NgModule({
   imports: [
     SharedModule,
     PartnersRoutingModule
   ],
-  declarations: [PartnerListComponent]
+  declarations: [PartnerListComponent, PartnerFormComponent]
 })
 export class PartnersModule { }
 
