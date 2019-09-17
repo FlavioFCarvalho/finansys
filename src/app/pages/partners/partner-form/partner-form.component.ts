fa656478cbfc10263519c44fbed85158dd2bc8d1
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-17 09:00:28 -0300

    Criação do métodos do partner-form

--- src/app/pages/partners/partner-form/partner-form.component.ts
+++ src/app/pages/partners/partner-form/partner-form.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { Validators } from "@angular/forms";
+
+
+import { PartnerService } from "../shared/partner.service";
+import { BaseResourceFormComponent } from 'src/app/shared/base-resource-form/base-resource-form.components';
+import { Partner } from '../shared/partner.model';
+
 
 @Component({
   selector: 'app-partner-form',
   templateUrl: './partner-form.component.html',
   styleUrls: ['./partner-form.component.css']
 })
-export class PartnerFormComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+export class PartnerFormComponent extends BaseResourceFormComponent<Partner> {
+
+  constructor(
+    protected partnerService: PartnerService, protected injector: Injector) {
+    super(injector, new Partner, partnerService, Partner.fromJson)
   }
 
+
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      type: [null],
+      cpf: [null],
+      cnpj: [null],
+      tipoPessoa: [null]
+
+    });
+  }
+
+  protected creationPageTitle(): string {
+    return "Cadastro de Novo Parceiro";
+  }
+
+  protected editionPageTitle(): string {
+    const partnerName = this.resource.name || "";
+    return "Editando Parceiro: " + partnerName;
+  }
 }
+
+
+
